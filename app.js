1c448dcac0852165f8d6adf9d619051ea4ba44d8
hind-a9 <85807816+hind-a9@users.noreply.github.com>
2026-03-29 15:59:48 +0300

    Set up HTTP server in app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello from DevSpaces demo!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("Hello from my DevSpaces project!");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
